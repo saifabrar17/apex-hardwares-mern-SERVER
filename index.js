40127dd3d54061e079211785091cf795f481b4ca
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-05-25 18:22:50 +0600

    added new product, delete product

--- index.js
+++ index.js
@@ -35,7 +35,26 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const product = await productCollection.findOne(query);
       res.send(product);
-  });
+    });
+
+    //ADD NEW PRODUCT
+    app.post('/product', async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    //DELETE AN EXISTING PRODUCT
+    
+    app.delete('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+  })
+
+
+
   }
 
   finally {
