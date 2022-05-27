c558dc1693462f2a661c4869816098a4c9686cc8
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-05-27 10:10:21 +0600

    jwt

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.acd3k.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run() {
   try {
     await client.connect();
@@ -23,6 +39,23 @@ async function run() {
     //database collections
     const productCollection = client.db('apex_hardwares').collection('products');
     const orderCollection = client.db('apex_hardwares').collection('orders');
+    const userCollection = client.db('apex_hardwares').collection('users');
+
+    //user collection api START
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
+    })
+    //user collection api END
 
 
     //get all products
@@ -65,7 +98,7 @@ async function run() {
     })
 
     //get all orders
-    app.get('/orders', async (req, res) => {
+    app.get('/orders', verifyJWT, async (req, res) => {
       const query = {};
       const cursor = orderCollection.find(query);
       const orders = await cursor.toArray();
